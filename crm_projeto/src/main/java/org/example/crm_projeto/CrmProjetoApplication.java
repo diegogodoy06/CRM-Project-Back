5835bbb08b32dda752b448b3c81b080191f396d2
joaouzeloto <joaoricardouzelotosilva2003@gmail.com>
2025-01-27 16:39:54 -0300

    -inicio do projeto
    projeto base

---
+++ crm_projeto/src/main/java/org/example/crm_projeto/CrmProjetoApplication.java
@@ -0,0 +1,13 @@
+package org.example.crm_projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmProjetoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmProjetoApplication.class, args);
+    }
+
+}
